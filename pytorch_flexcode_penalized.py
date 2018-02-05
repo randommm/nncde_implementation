bb4c899d66538cd605447fd4192e5bd66cb0bfac
Marco Inacio <m@marcoinacio.com>
2018-02-04 22:28:56 -0200

    penalized version

--- pytorch_flexcode_penalized.py
+++ pytorch_flexcode_penalized.py
@@ -61,7 +61,10 @@ np.random.seed(10)
 n_train = 100_000
 n_test = 800
 x_dim = 45
-ncomponents = 500
+ncomponents = 100
+
+slice_train1 = np.s_[:int(n_train*0.8)]
+slice_train2 = np.s_[int(n_train*0.8):]
 
 beta = stats.norm.rvs(size=x_dim, scale=0.2)
 func = lambda x: (np.dot(beta, x))
@@ -99,33 +102,78 @@ class NNFlexcode(nn.Module):
         super(NNFlexcode, self).__init__()
         self.fc1 = nn.Linear(x_dim, x_dim * 2)
         self.fc2 = nn.Linear(x_dim * 2, ncomponents)
+        self.penal_base = nn.Parameter(torch.Tensor([7.0]))
+        self.penal_expon = nn.Parameter(torch.Tensor([1.0]))
+        self.penalize = False
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        if self.penalize:
+            aranged = Variable(x.data.new(np.arange(ncomponents)))
+            x = x * F.sigmoid(self.penal_base) ** (aranged * self.penal_expon)
         return x
 
 nn_flexcode = NNFlexcode(x_dim, ncomponents)
+nn_flexcode.cuda()
 
-initial_params = list(nn_flexcode.parameters())
+initial_params = [x.clone().data.cpu() for x in nn_flexcode.parameters()]
 criterion = nn.MSELoss()
 
-nn_flexcode.cuda()
 nepoch = 30
 
 start_time = time.process_time()
 for epoch in range(nepoch):
     batch_size = min(10_000, 100 + 300 * int(epoch**1.3))
-    optimizer = optim.SGD(nn_flexcode.parameters(), lr=.01/(1+epoch))
-    permutation = torch.randperm(target.shape[0])
+    optimizer = optim.SGD(nn_flexcode.parameters(), lr=.1/(1+epoch))
+    permutation = torch.randperm(target[slice_train1].shape[0])
 
-    inputv_perm = inputv.data[permutation]
-    target_perm = target.data[permutation]
+    inputv_perm = inputv[slice_train1].data[permutation]
+    target_perm = target[slice_train1].data[permutation]
     inputv_perm = Variable(inputv_perm.pin_memory())
     target_perm = Variable(target_perm.pin_memory())
 
-    for i in range(0, target.shape[0] + batch_size, batch_size):
-        if i < target.shape[0]:
+    for i in range(0, target[slice_train1].shape[0] + batch_size, batch_size):
+        if i < target[slice_train1].shape[0]:
+            inputv_next = inputv_perm[i:i+batch_size].cuda(async=True)
+            target_next = target_perm[i:i+batch_size].cuda(async=True)
+
+        if i != 0:
+            optimizer.zero_grad()
+            output = nn_flexcode(inputv_this)
+            loss = criterion(output, target_this)
+            #loss = -2 * (output * target_this).sum(1).mean()
+            #loss += (Variable.mm(output, phi_grid)**2).mean()
+            loss.backward()
+            optimizer.step()
+            #print("Epoch", epoch, "and batch", i-1, "done", flush=True)
+
+        inputv_this = inputv_next
+        target_this = target_next
+    print("Epoch", epoch, "done", flush=True)
+
+elapsed_time = time.process_time() - start_time
+print("Elapsed time:", elapsed_time, flush=True)
+
+params_stage_1 = [x.clone().data.cpu() for x in nn_flexcode.parameters()]
+
+nn_flexcode.penalize = True
+nepoch = 50
+
+start_time = time.process_time()
+for epoch in range(nepoch):
+    batch_size = min(10_000, 100 + 300 * int(epoch**1.3))
+    optimizer = optim.SGD([nn_flexcode.penal_expon,
+                           nn_flexcode.penal_base], lr=1/(1+epoch))
+    permutation = torch.randperm(target[slice_train2].shape[0])
+
+    inputv_perm = inputv[slice_train2].data[permutation]
+    target_perm = target[slice_train2].data[permutation]
+    inputv_perm = Variable(inputv_perm.pin_memory())
+    target_perm = Variable(target_perm.pin_memory())
+
+    for i in range(0, target[slice_train2].shape[0] + batch_size, batch_size):
+        if i < target[slice_train2].shape[0]:
             inputv_next = inputv_perm[i:i+batch_size].cuda(async=True)
             target_next = target_perm[i:i+batch_size].cuda(async=True)
 
@@ -146,7 +194,7 @@ for epoch in range(nepoch):
 elapsed_time = time.process_time() - start_time
 print("Elapsed time:", elapsed_time, flush=True)
 
-params = list(nn_flexcode.parameters())
+params_stage_2 = [x.clone().data.cpu() for x in nn_flexcode.parameters()]
 
 nn_flexcode.cpu()
 phi_grid = phi_grid.cpu()
